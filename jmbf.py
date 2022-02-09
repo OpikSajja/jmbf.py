5d6f60d899f19cba2242a248b0bb039d3ebb24c3
OpikSajja <97097135+OpikSajja@users.noreply.github.com>
2022-02-09 22:14:38 +0700

    Update jmbf.py

--- jmbf.py
+++ jmbf.py
@@ -177,7 +177,7 @@ foto_2 = (f"""{c}     __   __  ___           __  ___   ___    ____{k}
 logo_epep = pilih([foto_2, foto_1])
 logo = (f"""
 {logo_epep}{Q}
-[++] Athour       : {Rk.opik}
+[++] Athour       : {Rk.OPIK.XD}
 [++] WhatsApp     : {opik_hp}""")
 
 try:
